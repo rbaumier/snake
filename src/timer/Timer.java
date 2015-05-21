2a6ce7a2bd5f7ad394d6a431c7f7185900d68ea8
Romain Baumier <baumier.romain@gmail.com>
2015-05-21 15:59:03 +0200

    refactor + add Timer backbone

--- src/timer/Timer.java
+++ src/timer/Timer.java
@@ -1,5 +1,29 @@
 package timer;
 
-public class Timer extends Thread {
+import android.os.Handler;
+
+public class Timer implements Runnable {
+  public Handler handler;
+  private boolean running = true;
+
+  public Timer() {
+  }
+
+  public void stop() {
+    running = false;
+  }
+
+  @Override
+  public void run() {
+    while (running) {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException ex) {
+        Thread.currentThread().interrupt();
+      }
+      handler.sendEmptyMessage(0); // send tick
+    }
+  }
 
 }
+
