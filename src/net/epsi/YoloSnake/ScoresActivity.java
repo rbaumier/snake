79c91ffb9d1fea51ed65ab1bd6420486e0cbe2b6
Romain Baumier <baumier.romain@gmail.com>
2015-05-30 13:26:07 +0200

    sort players by score and take the first 10

--- src/net/epsi/YoloSnake/ScoresActivity.java
+++ src/net/epsi/YoloSnake/ScoresActivity.java
@@ -8,8 +8,13 @@ import database.DAO;
 import models.Player;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class ScoresActivity extends Activity {
+  private static final int maxBestPlayers = 10;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -19,22 +24,30 @@ public class ScoresActivity extends Activity {
 
   private void showScores() {
     ArrayList<Player> players = DAO.getPlayers();
-    String[] values = formatPlayers(players);
+    Collections.sort(players, new Comparator<Player>() {
+      public int compare(Player p1, Player p2) {
+        return p2.score - p1.score;
+      }
+    });
 
     ListView scoresList = (ListView) findViewById(R.id.scoresList);
+    String[] bestPlayers = formatPlayers(
+      players.subList(0, Math.min(players.size(), maxBestPlayers ))
+    );
+
     scoresList.setAdapter(new ArrayAdapter<String>(
       this,
       android.R.layout.simple_list_item_1,
       android.R.id.text1,
-      values
+      bestPlayers
     ));
   }
 
-  private String[] formatPlayers(ArrayList<Player> players) {
+  private String[] formatPlayers(List<Player> players) {
     String[] scores = new String[players.size()];
-    for(int i = 0; i < players.size(); i++) {
+    for (int i = 0; i < players.size(); i++) {
       Player current = players.get(i);
-      scores[i] = current.name + " - " + current.score;
+      scores[i] = i + 1 + ". " + current.name + " - " + current.score;
     }
     return scores;
   }
