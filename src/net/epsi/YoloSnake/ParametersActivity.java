50e25cd03b096b81d3be6747a2fad92d3afa6f85
Romain Baumier <baumier.romain@gmail.com>
2015-05-30 10:27:51 +0200

    reset scores with confirmation

--- src/net/epsi/YoloSnake/ParametersActivity.java
+++ src/net/epsi/YoloSnake/ParametersActivity.java
@@ -1,18 +1,22 @@
 package net.epsi.YoloSnake;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
 
 public class ParametersActivity extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.parameters);
+    final Activity self = this;
 
     SharedPreferences pref = getApplicationContext().getSharedPreferences("SnakePreferences", MODE_PRIVATE);
     SharedPreferences.Editor editor = pref.edit();
@@ -20,15 +24,29 @@ public class ParametersActivity extends Activity {
     Button musicButton = (Button) findViewById(R.id.music);
     Boolean musicValue = pref.getBoolean("music", true);
     musicButton.setText(musicButtonText(musicValue));
-
-    Button resetScores = (Button) findViewById(R.id.resetScores);
-
     musicButton.setOnClickListener(new Button.OnClickListener() {
       public void onClick(View v) {
         Boolean newMusicValue = !pref.getBoolean("music", false);
         editor.putBoolean("music", newMusicValue);
         editor.apply();
         musicButton.setText(musicButtonText(newMusicValue));
+      }
+    });
+
+    Button resetScores = (Button) findViewById(R.id.resetScores);
+    resetScores.setOnClickListener(new Button.OnClickListener() {
+      public void onClick(View v) {
+        new AlertDialog.Builder(self)
+          .setIcon(android.R.drawable.ic_dialog_alert)
+          .setTitle("Reset scores")
+          .setMessage("Are you sure you want to reset scores?")
+          .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+              editor.putStringSet("scores", Collections.emptySet());
+            }
+          })
+          .setNegativeButton("No", null)
+          .show();
       }
     });
   }
