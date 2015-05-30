a7f26425ac0316689801f7b9c25cf47b620702b2
Romain Baumier <baumier.romain@gmail.com>
2015-05-30 09:14:28 +0200

    refactor MyActivity

--- src/net/epsi/YoloSnake/MyActivity.java
+++ src/net/epsi/YoloSnake/MyActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import terminal.Terminal;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyActivity extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -14,28 +17,21 @@ public class MyActivity extends Activity {
     setContentView(R.layout.main);
     final Activity self = this;
 
+    Map<Button, Class> buttons = new HashMap<Button, Class>() {{
+      put((Button) findViewById(R.id.newGameButton), GameActivity.class);
+      put((Button) findViewById(R.id.parametersButton), ParametersActivity.class);
+      put((Button) findViewById(R.id.scoresButton), ScoresActivity.class);
+    }};
+
+    for (Map.Entry<Button, Class> button : buttons.entrySet()) {
+      button.getKey().setOnClickListener(new Button.OnClickListener() {
+        public void onClick(View v) {
+          self.startActivity(new Intent(self, button.getValue()));
+        }
+      });
+    }
+
     // To show console version of the snake;
-//    Terminal.main();
-
-    Button newGame = (Button) findViewById(R.id.newGameButton);
-    newGame.setOnClickListener(new Button.OnClickListener() {
-      public void onClick(View v) {
-        self.startActivity(new Intent(self, GameActivity.class));
-      }
-    });
-
-    Button parameters = (Button) findViewById(R.id.parametersButton);
-    parameters.setOnClickListener(new Button.OnClickListener() {
-      public void onClick(View v) {
-        self.startActivity(new Intent(self, ParametersActivity.class));
-      }
-    });
-
-    Button scores = (Button) findViewById(R.id.scoresButton);
-    scores.setOnClickListener(new Button.OnClickListener() {
-      public void onClick(View v) {
-        self.startActivity(new Intent(self, ScoresActivity.class));
-      }
-    });
+    Terminal.main();
   }
 }
